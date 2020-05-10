3b62eb0bf732523ea18551fe8141c88977ca3dc6
Victor Guedes <victorguedes68@hotmail.com>
2020-05-10 20:24:12 -0300

    Core conversor made

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React,{useState} from 'react';
 
 function App() {
   const [binary,setBinary] = useState("");
+  const [decimal,setDecimal] = useState("");
   const [message,setMessage] = useState("");
 
   function handleBinaryChange(e){
-    function getInputedValue(){
-      const arrayFromInput = e.target.value.split("");
-      return arrayFromInput[arrayFromInput.length - 1];
+    function getLastIndexFromValue(value){
+      const arrayFromValue = e.target.value.split("");
+      return arrayFromValue[arrayFromValue.length - 1];
     }
-    const inputedValue = getInputedValue();
+    const inputedValue = getLastIndexFromValue(e.target.value);
+    // console.log(inputedValue)
 
     if (inputedValue === "0" || inputedValue === "1"){
       setBinary(prevValue => prevValue + inputedValue);
+
+      function convertBinaryToDecimal(binaryValue){
+        // Separar o binário de ponta a cabo
+        const arrayFromBinary = binaryValue.split("");
+        // ["0","1","0"]
+        const base = 2;
+        let index = 0, decimal = 0;
+
+        for (let i = arrayFromBinary.length - 1; i >= 0; i--){
+          
+          if (arrayFromBinary[i] === "1"){
+            decimal += base ** index;
+          }
+          index++;
+        }
+
+        return decimal
+      }
+      
+      setDecimal(convertBinaryToDecimal(binary + inputedValue));
     }else{
       setMessage("Please put simply 0 or 1");
     }
@@ -21,7 +43,10 @@ function App() {
   return (
     <div className="App">
       <form id="app-form">
+        <label>Binary:</label>
         <input value={binary} onChange={handleBinaryChange}/>
+        <label>Decimal:</label>
+        <input value={decimal} readOnly/>
         <p>{message}</p>
       </form>
     </div>
