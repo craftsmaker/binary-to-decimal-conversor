c536d7420aae49ddac6d2b066b78fd9a2fb00bce
Victor Guedes <victorguedes68@hotmail.com>
2020-05-14 17:44:24 -0300

    Core funionality improved

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
 import React,{useState} from 'react';
+import "./styles.css";
 
 function App() {
   const [binary,setBinary] = useState("");
   const [decimal,setDecimal] = useState("");
   const [message,setMessage] = useState("");
 
-  function handleBinaryChange(e){
-    function getLastIndexFromValue(value){
-      const arrayFromValue = e.target.value.split("");
-      return arrayFromValue[arrayFromValue.length - 1];
-    }
-    const inputedValue = getLastIndexFromValue(e.target.value);
-    // console.log(inputedValue)
-
-    if (inputedValue === "0" || inputedValue === "1"){
-      setBinary(prevValue => prevValue + inputedValue);
-
-      function convertBinaryToDecimal(binaryValue){
-        // Separar o binário de ponta a cabo
-        const arrayFromBinary = binaryValue.split("");
-        // ["0","1","0"]
-        const base = 2;
-        let index = 0, decimal = 0;
-
-        for (let i = arrayFromBinary.length - 1; i >= 0; i--){
+  function handleBinaryChange({key,type}){
+    
+    if (type === "keydown"){
+       function convertBinaryToDecimal(binaryValue){
           
-          if (arrayFromBinary[i] === "1"){
-            decimal += base ** index;
-          }
-          index++;
-        }
+          const arrayFromBinary = binaryValue.split("");
+          
+          const base = 2;
+          let index = 0, decimal = 0;
 
-        return decimal
+          for (let i = arrayFromBinary.length - 1; i >= 0; i--){
+            
+            if (arrayFromBinary[i] === "1"){
+              decimal += base ** index;
+            }
+            index++;
+          }
+
+          return decimal
+        }
+      
+      if (key === "Backspace"){
+        setBinary(state => state.slice(0,state.length-1))
+        setDecimal(convertBinaryToDecimal(binary))
+        return;
       }
       
-      setDecimal(convertBinaryToDecimal(binary + inputedValue));
-    }else{
-      setMessage("Please put simply 0 or 1");
+      function getLastIndexFromValue(value){
+          const arrayFromValue = key.split("");
+          return arrayFromValue[arrayFromValue.length - 1];
+      }
+      const inputedValue = getLastIndexFromValue(key);
+
+      if (inputedValue === "0" || inputedValue === "1"){
+        setBinary(prevValue => prevValue + inputedValue);
+
+       
+        
+        setDecimal(convertBinaryToDecimal(binary + inputedValue));
+        }else{
+          setMessage("Please put simply 0 or 1");
+        }
     }
   }
 
   return (
     <div className="App">
       <form id="app-form">
-        <label>Binary:</label>
-        <input value={binary} onChange={handleBinaryChange}/>
-        <label>Decimal:</label>
-        <input value={decimal} readOnly/>
-        <p>{message}</p>
+        <div>
+          <div>
+            <label>Binary:</label>
+            <label>Decimal:</label>
+          </div>
+          <div>
+            <input value={binary} onKeyDown={handleBinaryChange} onChange={handleBinaryChange}/>
+            <input value={decimal} readOnly/>
+          </div>
+        </div>
+        <div>
+          <p>{message}</p>
+        </div>
       </form>
     </div>
   );
